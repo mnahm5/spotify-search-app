9781ece1a24201fcf470c5f427a3c4c37b01afb5
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-14 21:21:30 +1100

    015 Search Component  Service

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
@@ -10,7 +12,12 @@ import {AboutComponent} from './components/about/about.component';
 import {routing} from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
+    imports:      [
+      BrowserModule,
+      routing,
+      FormsModule,
+      HttpModule
+    ],
   declarations: [
     AppComponent,
     SearchComponent,
